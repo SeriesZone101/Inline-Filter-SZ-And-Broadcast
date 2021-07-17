e9c4c17669d1927eb28ce702c31d6790716b9657
Deepak V <85978656+SeriesZone101@users.noreply.github.com>
2021-07-17 10:07:51 +0530

    Update strings.py

--- InlineBot/strings.py
+++ InlineBot/strings.py
@@ -13,7 +13,7 @@ from InlineBot import (
 if CUSTOM_START_MESSAGE:
     START_MESSAGE = CUSTOM_START_MESSAGE
 else:
-    START_MESSAGE = """<b>Hello {mention},
+    START_MESSAGE = """<b>Hola 👋 {mention},
 
 I am an Inline Saver Bot, you can save inline filters and It can be use in any of your chats easily, Click help for more details</b> 
 """
@@ -50,9 +50,9 @@ HELP_MESSAGE = f"""<b><u>Main Available Commands</u></b>
 ABOUT_MESSAGE = f"""<b><u>ABOUT ME</u></b>
 
 <b>○ Maintained by : <a href='tg://user?id={OWNER_ID}'>This Person</a>
-○ Channel : <a href='https://t.me/CodeXBotz'>Code 𝕏 Botz</a>
-○ Support : <a href='https://t.me/CodeXBotzSupport'>Code 𝕏 Botz Support</a>
-○ Source Code : <a href='https://github.com/CodeXBotz/Inline-Filter-Bot'>Click here</a>
+○ Feedback : <a href='https://t.me/DeepakV111'>Developer</a>
+○ Support : <a href='https://t.me/DeepakV111'>Joey</a>
+○ Source Code : <a href='https://t.me/movieszzzone'>Click here</a>
 ○ Language : <a href='https://www.python.org/'>Python 3</a>
 ○ Library : <a href='https://github.com/pyrogram/pyrogram'>Pyrogram Asyncio {__version__}</a></b>
 """
@@ -80,16 +80,16 @@ MARKDOWN_HELP = """<b><u>Markdown Formatting</u></b>
     show as: <s>StrikeThrough Text</s>
     
 ○ <b>Hyper Link</b>
-    format: <code>[Text](https://t.me/CodeXBotz)</code>
-    show as: <a href='https://t.me/CodeXBotz'>Text</a>
+    format: <code>[Text](https://t.me/DeepakV111)</code>
+    show as: <a href='https://t.me/DeepakV111'>Text</a>
     
 ○ <b>Buttons</b>
     <u>Url Button</u>:
-    <code>[Button Text](buttonurl:https://t.me/CoddeXBotz)</code>
+    <code>[Button Text](buttonurl:https://t.me/DeepakV111)</code>
     <u>Alert Button</u>:
     <code>[Button Text](buttonalert:Alert Text)</code>
     <u>In Sameline</u>:
-    <code>[Button Text](buttonurl:https://t.me/CodeXBotz:same)</code></i>
+    <code>[Button Text](buttonurl:https://t.me/DeepakV111:same)</code></i>
 
 ○ <b>Notes:</b>
     <i>Keep every Buttons in Seperate line when formating</i>
